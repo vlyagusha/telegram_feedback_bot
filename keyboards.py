1826a625cf494e279b9a23d606ba4e1da9003f66
Vyacheslav Lyagusha <vlyagusha@gmail.com>
2021-12-06 20:29:06 +0300

    доработки

--- keyboards.py
+++ keyboards.py
@@ -7,12 +7,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 zones_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-zones = os.environ.get("ZONES").split(",")
+zones = os.environ.get("ZONES").split(";")
 for zone in zones:
     zones_kb.add(KeyboardButton('/z ' + zone))
 
 position_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-positions = os.environ.get("POSITIONS").split(",")
+positions = os.environ.get("POSITIONS").split(";")
 for position in positions:
     position_kb.add(KeyboardButton('/p ' + position))
 
--- main.py
+++ main.py
@@ -16,8 +16,13 @@ GROUP_CHAT_ID = os.environ.get("GROUP_CHAT_ID")
 feedbacks = {}
 
 
+@dp.message_handler(IDFilter(chat_id=CEO_CHAT_ID))
+async def sniff_ceo_chat(message: types.Message):
+    pass
+
+
 @dp.message_handler(IDFilter(chat_id=GROUP_CHAT_ID))
-async def sniff_chat(message: types.Message):
+async def sniff_group_chat(message: types.Message):
     me = await bot.get_me()
     await message.reply(f'''
 Добрый день, {message.from_user.full_name}, я бот с обратной связью к руководству.
@@ -46,12 +51,13 @@ async def get_zone(message):
 @dp.message_handler(commands=['p'])
 async def get_position(message):
     feedbacks[message.from_user.id].update({'position': message.get_args()})
-    await message.reply('Представьтесь', reply_markup=kb.blank_kb)
+    await message.reply('Напишите вашу фамилию', reply_markup=kb.blank_kb)
 
 
 @dp.message_handler()
 async def get_text(message):
     if message.from_user.id not in feedbacks:
+        feedbacks[message.from_user.id] = {}
         await message.reply('Выберите участок', reply_markup=kb.zones_kb)
         return
 
@@ -63,22 +69,37 @@ async def get_text(message):
         await message.reply('Выберите должность', reply_markup=kb.position_kb)
         return
 
-    if 'name' not in feedbacks[message.from_user.id]:
-        feedbacks[message.from_user.id].update({'name': message.text})
-        await message.reply('Опишите проблему которая у Вас возникла', reply_markup=kb.blank_kb)
+    if 'last_name' not in feedbacks[message.from_user.id]:
+        feedbacks[message.from_user.id].update({'last_name': message.text})
+        await message.reply('Напишите ваше имя', reply_markup=kb.blank_kb)
+        return
+
+    if 'first_name' not in feedbacks[message.from_user.id]:
+        feedbacks[message.from_user.id].update({'first_name': message.text})
+        await message.reply('Напишите ваше отчество', reply_markup=kb.blank_kb)
+        return
+
+    if 'middle_name' not in feedbacks[message.from_user.id]:
+        feedbacks[message.from_user.id].update({'middle_name': message.text})
+        await message.reply('Опишите проблему, которая у вас возникла', reply_markup=kb.blank_kb)
         return
 
     feedbacks[message.from_user.id].update({'message': message.text})
 
-    await message.reply('Спасибо за обращение!', reply_markup=kb.blank_kb)
+    await message.reply('''Спасибо за обращение!
+Если вы хотите оставить новое обращение, нажмите /start''', reply_markup=kb.blank_kb)
 
+    name = feedbacks[message.from_user.id]['last_name'] + ' ' +\
+        feedbacks[message.from_user.id]['first_name'] + ' ' +\
+        feedbacks[message.from_user.id]['middle_name']
     await bot.send_message(CEO_CHAT_ID, f'''
-Добрый день! Новое сообщение от сотрудника.
+Новое сообщение от сотрудника.
 Участок: {feedbacks[message.from_user.id]['zone']}
 Должность: {feedbacks[message.from_user.id]['position']}
-ФИО: {feedbacks[message.from_user.id]['name']}
+ФИО: {name}
 Сообщение: {feedbacks[message.from_user.id]['message']} 
 ''', reply_markup=kb.blank_kb)
+    del feedbacks[message.from_user.id]
 
 
 if __name__ == '__main__':
